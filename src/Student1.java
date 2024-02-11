10b47acf208d9c58967b3b38147ab1c10f014c78
Dinesh Adabala <dinesh.adabala417@gmail.com>
2024-02-11 11:19:11 +0530

    hai from student1

---
+++ src/Student1.java
@@ -0,0 +1,9 @@
+public class Student1 {
+    public String name;
+    public String fatherName;
+
+    public Student1(String name, String fatherName) {
+        this.name = name;
+        this.fatherName = fatherName;
+    }
+}
